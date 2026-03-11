46f20f89aa3c359edf7eb1613217c3b704185b14
christofferkjellow-andersen <Christofferka@gmail.com>
2026-03-11 11:40:16 +0100

    docker setup

--- src/main/java/org/example/kinobackend/KinobackendApplication.java
+++ src/main/java/org/example/kinobackend/KinobackendApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class KInobackendApplication {
+public class KinobackendApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(KInobackendApplication.class, args);
+        SpringApplication.run(KinobackendApplication.class, args);
     }
 
 }
--- src/main/java/org/example/kinobackend/Model/Category.java
+++ src/main/java/org/example/kinobackend/Model/Category.java
@@ -1,4 +1,22 @@
 package org.example.kinobackend.Model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "categories")
 public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    private String name;
+
+    public Category() {}
+    public Category(String name) { this.name = name; }
+
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
 }
