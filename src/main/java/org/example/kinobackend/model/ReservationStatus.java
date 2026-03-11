2e8cb08e06e1161ea757c93831300e929932492e
Aminrz123 <moay0001@stud.kea.dk>
2026-03-11 12:18:23 +0100

    fixet status

--- src/main/java/org/example/kinobackend/model/ReservationStatus.java
+++ src/main/java/org/example/kinobackend/model/ReservationStatus.java
@@ -1,18 +1,8 @@
 package org.example.kinobackend.model;
 
-<<<<<<< HEAD
 public enum ReservationStatus {
     PENDING,
     CONFIRMED,
     CANCELLED,
     CONVERTED
 }
-=======
-
-    public enum ReservationStatus {
-        PENDING,
-        CONFIRMED,
-        CANCELLED,
-        CONVERTED
-    }
->>>>>>> 34a0f8a0bfb5d092f6b727a4aadb578aa7481940
