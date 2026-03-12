ccafaa60b8ca57c34d018da95362d528261cc2e9
zaka23-cyber <petererperker2@gmail.com>
2026-03-12 11:47:37 +0100

    Main

--- src/main/java/org/example/kinobackend/RestController/TheatreController.java
+++ src/main/java/org/example/kinobackend/RestController/TheatreController.java
@@ -1,4 +1,4 @@
-package org.example.kinobackend.RestControllers;
+package org.example.kinobackend.RestController;
 
 import org.example.kinobackend.model.Theatre;
 import org.example.kinobackend.service.TheatreService;
